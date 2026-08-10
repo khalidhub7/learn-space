65d11a145cb77569d74e864187b2f21060d1bc08
khalidhub7 <louguikhalid7@gmail.com>
2026-08-10 17:04:02 +0100

    sprint 2

--- components/motion/sprint-2.jsx
+++ components/motion/sprint-2.jsx
@@ -1,65 +1,67 @@
 import { motion } from "motion/react";
 
-// Initial + Animate (Slide-in + Fade-in)
+/* 
+whileHover, whileTap, whileFocus, whileDrag, whileInView
+*/
 const Sprint2 = () => {
   return (
     <>
-    {/* 2 — Hover (Hover Scale) */}
-        <div className="space-y-3">
-          <p>2. Hover</p>
+      {/* 2 — Hover (Hover Scale) */}
+      <div className="space-y-3">
+        <p>2. Hover</p>
 
-          <motion.div className="motion-div" whileHover={{ scale: 1.1 }}>
-            Hello
-          </motion.div>
-        </div>
+        <motion.div className="motion-div" whileHover={{ scale: 1.1 }}>
+          Hello
+        </motion.div>
+      </div>
 
-        {/* 3 — Tap (Tap Shrink) */}
-        <div className="space-y-3">
-          <p>3. Tap</p>
+      {/* 3 — Tap (Tap Shrink) */}
+      <div className="space-y-3">
+        <p>3. Tap</p>
 
-          <motion.div className="motion-div" whileTap={{ scale: 0.9 }}>
-            Hello
-          </motion.div>
-        </div>
+        <motion.div className="motion-div" whileTap={{ scale: 0.9 }}>
+          Hello
+        </motion.div>
+      </div>
 
-        {/* 4 — Focus (Focus Highlight) */}
-        <div className="space-y-3">
-          <p>4. Focus</p>
+      {/* 4 — Focus (Focus Highlight) */}
+      <div className="space-y-3">
+        <p>4. Focus</p>
 
-          <motion.button
-            className="motion-div"
-            whileFocus={{ backgroundColor: "#fefce8" }}
-          >
-            Focus me
-          </motion.button>
-        </div>
+        <motion.button
+          className="motion-div"
+          whileFocus={{ backgroundColor: "#fefce8" }}
+        >
+          Focus me
+        </motion.button>
+      </div>
 
-        {/* 5 — Drag */}
-        <div className="space-y-3">
-          <p>5. Drag</p>
+      {/* 5 — Drag */}
+      <div className="space-y-3">
+        <p>5. Drag</p>
 
-          <motion.div
-            className="motion-div cursor-grab"
-            drag
-            whileDrag={{ border: "2px dotted black" }}
-          >
-            Drag me
-          </motion.div>
-        </div>
+        <motion.div
+          className="motion-div cursor-grab"
+          drag
+          whileDrag={{ border: "2px dotted black" }}
+        >
+          Drag me
+        </motion.div>
+      </div>
 
-        {/* 6 — While In View (Fade-in on View) */}
-        <div className="space-y-3">
-          <p>6. While In View</p>
+      {/* 6 — While In View (Fade-in on View) */}
+      <div className="space-y-3">
+        <p>6. While In View</p>
 
-          <motion.div
-            className="motion-div"
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            Hello
-          </motion.div>
-        </div>
+        <motion.div
+          className="motion-div"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+        >
+          Hello
+        </motion.div>
+      </div>
     </>
   );
 };
--- pages/motion/index.jsx
+++ pages/motion/index.jsx
@@ -1,10 +1,12 @@
 import { Sprint1 } from "@/components/motion/sprint-1";
+import { Sprint2 } from "@/components/motion/sprint-2";
 
 const Home = () => {
   return (
     <main className="p-10">
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        <Sprint1 />
+        {/* <Sprint1 /> */}
+        <Sprint2 />
       </div>
     </main>
   );
