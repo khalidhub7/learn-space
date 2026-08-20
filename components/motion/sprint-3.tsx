cb89048a2b62c4768f14b37142253e7eaa09c240
khalidhub7 <louguikhalid7@gmail.com>
2026-08-20 19:39:30 +0100

    Variants

--- components/motion/sprint-3.tsx
+++ components/motion/sprint-3.tsx
@@ -19,39 +19,45 @@ const Sprint3 = () => {
     >
       <motion.button
         className="
-      rounded-full w-8 aspect-square
-      flex items-center justify-center
-      ring-2 ring-slate-200 
-      ring-offset-1 ring-offset-slate-300
-      "
+        rounded-full w-8 aspect-square cursor-pointer
+        flex items-center justify-center
+        ring-2 ring-slate-200
+        ring-offset-1 ring-offset-slate-300
+        "
+        whileTap={{ scale: 0.9 }}
+        whileHover={{ rotate: 360 }}
+        transition={{ type: "spring", stiffness: 300 }}
+        onClick={() => setIsOpen(!isOpen)}
       >
         {isOpen ? "✕" : "☰"}
       </motion.button>
 
-      <nav
-        className="
+      {isOpen ? (
+        <nav
+          className="
         p-4 rounded
-        ring-2 ring-gray-100
+        ring-2 ring-gray-200
         ring-offset-1 ring-offset-fuchsia-300
         "
-      >
-        <ul className="w-sm flex flex-col items-center gap-5 ">
-          {testNavs.map((l) => (
-            <li
-              key={l}
-              className="
+        >
+          <ul className="w-sm flex flex-col items-center gap-5">
+            {testNavs.map((l) => (
+              <li
+                key={l}
+                className="
               rounded p-2 w-64
               ring-2 ring-slate-200
               ring-offset-2 ring-offset-slate-300
               "
-            >
-              <a href="" className="text-center block">
-                {l}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+              >
+                <a href="" className="text-center block">
+                  {l}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      ) : undefined}
     </div>
   );
 };
