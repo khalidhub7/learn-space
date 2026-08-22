a1cd86f25f657a36f3784413715365f9f842b57f
khalidhub7 <louguikhalid7@gmail.com>
2026-08-22 02:14:21 +0100

    Variants

--- components/motion/sprint-3.tsx
+++ components/motion/sprint-3.tsx
@@ -15,7 +15,7 @@ const variants: Variants = {
 
 const parentVariants: Variants = {
   visible: { transition: { staggerChildren: 0.2 } },
-  exit: { opacity: 0, transition: { duration: 1 } },
+  exit: { opacity: 0, transition: { duration: 1  } },
 };
 
 const Sprint3 = () => {
