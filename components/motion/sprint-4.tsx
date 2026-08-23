c16098f627081c240d5d9645c0baad77ab4f59c3
khalidhub7 <louguikhalid7@gmail.com>
2026-08-23 16:13:00 +0100

    Layout animations

--- components/motion/sprint-4.tsx
+++ components/motion/sprint-4.tsx
@@ -23,24 +23,23 @@ const Sprint4 = () => {
     <div className="space-y-10">
       <div
         className="
-      relative
-      w-xl rounded-lg p-5 
-      ring-4 ring-gray-100
-      flex flex-col items-center gap-10
+        relative w-xl rounded-lg p-5
+        ring-4 ring-gray-100 
+        flex flex-col items-center gap-10
         "
       >
         <motion.button
           className="absolute -top-8 right-5 cursor-pointer text-xl"
-          whileHover={{ scale: 1.3, rotate: 360 }}
+          whileHover={{ scale: 1.3, rotate: 90 }}
           onClick={() => setHiddenIds(new Set())}
         >
           ↻
         </motion.button>
         <ul
           className="
-       w-full p-2 grid grid-cols-3 gap-y-10
-       place-items-center
-      "
+          w-full p-2 grid grid-cols-3 gap-y-10
+          place-items-center
+          "
         >
           {widgets.map((w) => (
             <motion.li
@@ -49,12 +48,12 @@ const Sprint4 = () => {
 
               className={`
                 relative w-28
-            flex items-center justify-center
-            aspect-square rounded-lg
-            ring-2 ring-slate-200
-            ring-offset-1 ring-offset-slate-300
-            ${hiddenIds.has(w.id) ? "hidden" : ""}
-            `}
+                flex items-center justify-center
+                aspect-square rounded-lg
+                ring-2 ring-slate-200
+                ring-offset-1 ring-offset-slate-300
+                ${hiddenIds.has(w.id) ? "hidden" : ""}
+                `}
             >
               <motion.button
                 className=" cursor-pointer absolute right-2 top-1 "
