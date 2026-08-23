91cfdca04febfeb4c9d4ed3c74151fc7f4cdccf8
khalidhub7 <louguikhalid7@gmail.com>
2026-08-23 06:05:40 +0100

    Layout animations

--- components/motion/sprint-4.tsx
+++ components/motion/sprint-4.tsx
@@ -9,52 +9,98 @@ const widgets = [
   { id: 1, name: "Weather" },
   { id: 2, name: "Clock" },
   { id: 3, name: "Calendar" },
-  /* { id: 4, name: "Notes" },
+  { id: 4, name: "Notes" },
   { id: 5, name: "Tasks" },
-  { id: 6, name: "Music" }, */
+  { id: 6, name: "Music" },
 ];
+const tabs = ["Home", "About", "Projects"];
 
 const Sprint4 = () => {
   const [hiddenIds, setHiddenIds] = useState(new Set([]));
+  const [active, setActive] = useState(null);
 
   return (
-    <div
-      className="
-      w-xl rounded-lg p-5 h-96
+    <div className="space-y-10">
+      <div
+        className="
+      relative
+      w-xl rounded-lg p-5 
       ring-4 ring-gray-100
       flex flex-col items-center gap-10
         "
-    >
-      <ul
-        className="
-       w-full p-2 grid grid-cols-3 gap-3
-      "
       >
-        {widgets.map((w) => (
-          <motion.li
-            layout
-            key={w.id}
+        <motion.button
+          className="absolute -top-8 right-5 cursor-pointer text-xl"
+          whileHover={{ scale: 1.3, rotate: 360 }}
+          onClick={() => setHiddenIds(new Set())}
+        >
+          ↻
+        </motion.button>
+        <ul
+          className="
+       w-full p-2 grid grid-cols-3 gap-y-10
+       place-items-center
+      "
+        >
+          {widgets.map((w) => (
+            <motion.li
+              layout
+              key={w.id}
 
-            className={`
-                relative
+              className={`
+                relative w-28
             flex items-center justify-center
             aspect-square rounded-lg
             ring-2 ring-slate-200
             ring-offset-1 ring-offset-slate-300
             ${hiddenIds.has(w.id) ? "hidden" : ""}
             `}
-          >
-            <motion.button
-              className=" cursor-pointer absolute right-2 top-1 "
-              whileHover={{ scale: 1.3, rotate: 90 }}
-              onClick={() => setHiddenIds((prev) => new Set(prev).add(w.id))}
             >
-              ×
-            </motion.button>
-            <p> {w.name} </p>
-          </motion.li>
-        ))}
-      </ul>
+              <motion.button
+                className=" cursor-pointer absolute right-2 top-1 "
+                whileHover={{ scale: 1.3, rotate: 90 }}
+                onClick={() => setHiddenIds((prev) => new Set(prev).add(w.id))}
+              >
+                ×
+              </motion.button>
+              <p> {w.name} </p>
+            </motion.li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className="
+      relative
+      w-xl rounded-lg p-5 
+      ring-4 ring-gray-100
+      flex flex-col items-center gap-10
+        "
+      >
+        <ul className="flex justify-around w-full">
+          {tabs.map((tab) => (
+            <li>
+              <button
+                onClick={() => setActive(tab)}
+                className="relative cursor-pointer"
+              >
+                {tab}
+                {active === tab && (
+                  <motion.div
+                    layoutId="underline"
+                    className="
+                    absolute -top-1 -left-2
+                    rounded-lg h-8 w-20
+                    ring-2 ring-slate-200
+                    ring-offset-1 ring-offset-slate-400
+                  "
+                  />
+                )}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
