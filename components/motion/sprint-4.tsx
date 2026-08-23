afb6de6ded487246db223b563d7630f239709aaa
khalidhub7 <louguikhalid7@gmail.com>
2026-08-23 02:46:51 +0100

    Layout animations

--- components/motion/sprint-4.tsx
+++ components/motion/sprint-4.tsx
@@ -1,19 +1,22 @@
 import { motion } from "motion/react";
+import { useState } from "react";
 
 /* 
 sprint 4: Layout animations
 */
 
 const widgets = [
-  { id: "1", name: "Weather" },
-  { id: "2", name: "Clock" },
-  { id: "3", name: "Calendar" },
-  /* { id: "4", name: "Notes" },
-  { id: "5", name: "Tasks" },
-  { id: "6", name: "Music" }, */
+  { id: 1, name: "Weather" },
+  { id: 2, name: "Clock" },
+  { id: 3, name: "Calendar" },
+  /* { id: 4, name: "Notes" },
+  { id: 5, name: "Tasks" },
+  { id: 6, name: "Music" }, */
 ];
 
 const Sprint4 = () => {
+  const [hiddenIds, setHiddenIds] = useState(new Set([]));
+
   return (
     <div
       className="
@@ -29,17 +32,25 @@ const Sprint4 = () => {
       >
         {widgets.map((w) => (
           <motion.li
+            layout
             key={w.id}
-            
-            className="
-            relative
+
+            className={`
+                relative
             flex items-center justify-center
             aspect-square rounded-lg
             ring-2 ring-slate-200
             ring-offset-1 ring-offset-slate-300
-            "
+            ${hiddenIds.has(w.id) ? "hidden" : ""}
+            `}
           >
-            <span className="absolute right-2 top-1 " >x</span>
+            <motion.button
+              className=" cursor-pointer absolute right-2 top-1 "
+              whileHover={{ scale: 1.3, rotate: 90 }}
+              onClick={() => setHiddenIds((prev) => new Set(prev).add(w.id))}
+            >
+              ×
+            </motion.button>
             <p> {w.name} </p>
           </motion.li>
         ))}
