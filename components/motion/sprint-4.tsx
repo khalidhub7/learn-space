149f05b55eca3afde406a702f966420541d8f5d2
khalidhub7 <louguikhalid7@gmail.com>
2026-08-26 05:52:17 +0100

    Layout animations

--- components/motion/sprint-4.tsx
+++ components/motion/sprint-4.tsx
@@ -44,10 +44,10 @@ const NavItem = ({ tabName, isActive, setActive }) => {
           layoutId="active-tab"
           transition={{ type: "spring", stiffness: 300, damping: 20 }}
           className="
-            absolute -top-1 -left-2 -z-10
-            bg-neutral-50 rounded h-8 w-20
+            absolute -top-1 -left-4 -z-10
+            bg-neutral-50 rounded-full h-8 w-24
             ring-2 ring-olive-200
-            ring-offset-1 ring-offset-fuchsia-400
+            ring-offset-1 ring-offset-blue-400
             "
         />
       ) : undefined}
@@ -88,32 +88,32 @@ const Sprint4 = () => {
       </ul>
 
       {/* layout */}
-      {
-        concept === "layout" ?  <div
-        className="
+      {concept === "layout" ? (
+        <div
+          className="
         relative w-xl rounded-lg p-5
         ring-4 ring-gray-100
         "
-      >
-        <motion.button
-          className="absolute -top-8 right-5 cursor-pointer text-xl"
-          whileHover={{ scale: 1.3, rotate: 90 }}
-          onClick={() => setHiddenIds(new Set())}
         >
-          ↻
-        </motion.button>
-        <ul
-          className="
+          <motion.button
+            className="absolute -top-8 right-5 cursor-pointer text-xl"
+            whileHover={{ scale: 1.3, rotate: 90 }}
+            onClick={() => setHiddenIds(new Set())}
+          >
+            ↻
+          </motion.button>
+          <ul
+            className="
           w-full p-2 grid grid-cols-3 gap-y-10
           place-items-center
           "
-        >
-          {widgets.map((w) => (
-            <motion.li
-              layout
-              key={w.id}
+          >
+            {widgets.map((w) => (
+              <motion.li
+                layout
+                key={w.id}
 
-              className={`
+                className={`
                 relative w-28
                 flex items-center justify-center
                 aspect-square rounded-lg
@@ -121,92 +121,89 @@ const Sprint4 = () => {
                 ring-offset-1 ring-offset-slate-300
                 ${hiddenIds.has(w.id) ? "hidden" : ""}
                 `}
-            >
-              <motion.button
-                className=" cursor-pointer absolute right-2 top-1 "
-                whileHover={{ scale: 1.3, rotate: 90 }}
-                onClick={() => setHiddenIds((prev) => new Set(prev).add(w.id))}
               >
-                ×
-              </motion.button>
-              <p> {w.name} </p>
-            </motion.li>
-          ))}
-        </ul>
-      </div> : undefined
-      }
+                <motion.button
+                  className=" cursor-pointer absolute right-2 top-1 "
+                  whileHover={{ scale: 1.3, rotate: 90 }}
+                  onClick={() =>
+                    setHiddenIds((prev) => new Set(prev).add(w.id))
+                  }
+                >
+                  ×
+                </motion.button>
+                <p> {w.name} </p>
+              </motion.li>
+            ))}
+          </ul>
+        </div>
+      ) : undefined}
 
       {/* layoutId */}
 
-      {/* <div
-        className="
+      {concept === "layoutId" ? (
+        <div
+          className="
         relative w-xl rounded-lg p-5
         ring-4 ring-gray-100
         "
-      >
-        <ul className="flex justify-around w-full">
-          {tabs.map((tab) => (
-            <li key={tab} className="relative isolate">
-              <NavItem
-                tabName={tab}
-                isActive={activeTab === tab}
-                setActive={setActiveTab}
-              />
-            </li>
-          ))}
-        </ul>
-      </div> */}
+        >
+          <ul className="flex justify-around w-full">
+            {tabs.map((tab) => (
+              <li key={tab} className="relative isolate">
+                <NavItem
+                  tabName={tab}
+                  isActive={activeTab === tab}
+                  setActive={setActiveTab}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : undefined}
 
       {/* LayoutGroup */}
-      {/* <div
-        className="
+
+      {concept === "LayoutGroup" ? (
+        <div
+          className="
         w-xl rounded-lg p-5
         ring-4 ring-gray-100
         space-y-1
         "
-      >
-        
-        Test:
-        1. Open item 2.
-        2. Quickly toggle item 1.
-        Without LayoutGroup:
-        Animations are separate, so you may see overlap.
-        With LayoutGroup:
-        Animations are coordinated, so the overlap is avoided.
-        Rule:
-        LayoutGroup is useful when items have separate state/updates.
-       
-
-        <p className="text-blue-400 font-bold text-xl ">
-          LayoutGroup example 1
-        </p>
-
-        <p className="text-slate-400">
-          with/without LayoutGroup read cmnts inside code to understand
-        </p>
-
-        <div className="grid grid-cols-2 gap-x-3">
-          <div className="space-y-5">
-            <p className="text-fuchsia-500">without LayoutGroup</p>
-            <div>
-              {tabs.map((t) => (
-                <AccordionItem header={t} />
-              ))}
-            </div>
-          </div>
-
-          <div className="space-y-5">
-            <p className="text-fuchsia-500">with LayoutGroup</p>
-            <div>
-              <LayoutGroup>
+        >
+          Test: 1. Open item 2. 2. Quickly toggle item 1. Without LayoutGroup:
+          Animations are separate, so you may see overlap. With LayoutGroup:
+          Animations are coordinated, so the overlap is avoided. Rule:
+          LayoutGroup is useful when items have separate state/updates.
+          <p className="text-blue-400 font-bold text-xl ">
+            LayoutGroup example 1
+          </p>
+          <p className="text-slate-400">
+            with/without LayoutGroup read cmnts inside code to understand
+          </p>
+          <div className="grid grid-cols-2 gap-x-3">
+            <div className="space-y-5">
+              <p className="text-fuchsia-500">without LayoutGroup</p>
+              <div>
                 {tabs.map((t) => (
                   <AccordionItem header={t} />
                 ))}
-              </LayoutGroup>
+              </div>
+            </div>
+
+            <div className="space-y-5">
+              <p className="text-fuchsia-500">with LayoutGroup</p>
+              <div>
+                <LayoutGroup>
+                  {tabs.map((t) => (
+                    <AccordionItem header={t} />
+                  ))}
+                </LayoutGroup>
+              </div>
             </div>
           </div>
         </div>
-      </div> */}
+      ) : undefined}
     </div>
   );
 };
