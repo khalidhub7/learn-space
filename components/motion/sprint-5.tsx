0fd877cf8d6d6b1a57f0db7a34e918937030675c
khalidhub7 <louguikhalid7@gmail.com>
2026-08-27 03:54:56 +0100

    Motion Values + Scroll

--- components/motion/sprint-5.tsx
+++ components/motion/sprint-5.tsx
@@ -41,7 +41,15 @@ const Sprint5 = () => {
       </ul>
 
       {concept === "useMotionValue" ? (
-        <div className="w-xl rounded-lg p-5 ring-4 ring-gray-100">
+        <div
+          className="
+        w-xl rounded-lg p-5 ring-4 ring-gray-100
+        flex flex-col gap-6 items-center justify-center
+        "
+        >
+          <p className="text-slate-500">
+            click me ( change x without re-render )
+          </p>
           <motion.div
             className="bg-blue-400 size-10 rounded-xl cursor-pointer"
             onClick={() => (x.get() === 0 ? x.set(200) : x.set(0))}
@@ -57,7 +65,7 @@ const Sprint5 = () => {
         flex flex-col gap-6 items-center justify-center
         "
         >
-          <p className="text-zinc-600">
+          <p className="text-slate-500">
             drag me ( opacity caculated according x )
           </p>
           <motion.div
