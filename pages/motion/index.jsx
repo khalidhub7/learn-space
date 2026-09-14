488c89925ece546554ae0e8d714c856d4adc549c
khalidhub7 <louguikhalid7@gmail.com>
2026-09-14 01:47:16 +0100

    Variants, AnimatePresence, Exit animations

--- pages/motion/index.jsx
+++ pages/motion/index.jsx
@@ -42,7 +42,7 @@ const sprints = [
 ];
 
 const Motion = () => {
-  const [activeSprintId, setActiveSprintId] = useState(2);
+  const [activeSprintId, setActiveSprintId] = useState(3);
   const activeSprint = sprints[activeSprintId - 1];
   const ActiveComponent = activeSprint.component;
 
